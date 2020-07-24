64a8c973e60118481789a68984638a886f161c49
ismam <ishmamalhoque@gmail.com>
2020-07-25 04:07:59 +0800

    installed opencv,imported data collection

--- app/src/main/java/com/example/capstoneappdraft/MainActivity.java
+++ app/src/main/java/com/example/capstoneappdraft/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.capstoneappdraft;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -51,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }, TIME_OUT);
     }
+
 }
